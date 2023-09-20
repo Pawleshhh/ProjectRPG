90da2fe8e9c781ab3d931685698980a53f6ddcac
Mateusz Pawlikowski (MTWP) <mtwp@demant.com>
2023-09-20 13:23:41 +0200

    RNG implementation

---
+++ Source/ProjectRPG.Core/Implementation/Random/DefaultRng.cs
@@ -0,0 +1,48 @@
+﻿namespace ProjectRPG.Core;
+
+internal class DefaultRng : IRandomNumberGenerator
+{
+
+    private Random? random;
+    private Random Random => random ??= new Random(Seed);
+
+    public DefaultRng() { }
+
+    public DefaultRng(int seed)
+    {
+        Seed = seed;
+    }
+
+    public int Seed { get; private set; }
+
+    public double NextDouble()
+    {
+        return Random.NextDouble();
+    }
+
+    public int NextInt()
+    {
+        return Random.Next();
+    }
+
+    public int NextInt(int maxValue)
+    {
+        return Random.Next(maxValue);
+    }
+
+    public int NextInt(int minValue, int maxValue)
+    {
+        return Random.Next(minValue, maxValue);
+    }
+
+    public void Reset()
+    {
+        this.random = null;
+    }
+
+    public void Reset(int seed)
+    {
+        Seed = seed;
+        Reset();
+    }
+}
